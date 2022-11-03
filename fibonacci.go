99c44d1262e0336ade6d80d3bf70562f4ebde361
Maksim Gribkov <80826818+MaximGribkov@users.noreply.github.com>
2022-11-03 15:56:21 +0300

    learning recursion)

---
+++ fibonacci.go
@@ -0,0 +1,20 @@
+package main
+
+import ("fmt")
+
+func main() {
+  var a int
+  fmt.Scan(&a)
+  fmt.Println(fibonacci(a))
+}
+
+func fibonacci(n int) int {
+  if n == 0 {
+    return 0
+  }
+  if n == 1 {
+    return 1
+  } else {
+  return fibonacci(n - 1) + fibonacci(n - 2)
+  }
+}
